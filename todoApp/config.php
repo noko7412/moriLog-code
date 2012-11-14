49bf82a7368af26e21b5a945f03e44ffc7091c42
norihiko <norihiko.moriyama@stitch.co.jp>
2012-11-14 13:22:55 +0900

    修正

--- todoApp/config.php
+++ todoApp/config.php
@@ -1,13 +1,11 @@
 <?php
 // DB
-
 define('DB_HOST', '*****');
 define('DB_NAME', '*****');
 define('DB_USER', '*****');
 define('DB_PASSWORD', '*****');
-define('DSN', 'mysql:host=DB_HOST;dbname=DB_NAME');
+define('DSN', 'mysql:host='. DB_HOST .';dbname='. DB_NAME);
 
 //エラー表示
 error_reporting(E_ALL);
-
 
